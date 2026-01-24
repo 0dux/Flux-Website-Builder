97ab6e8f229ab85d5994461a895363b70122004f
Daksh Yadav <dakshdav@gmail.com>
2026-01-24 21:57:05 +0530

    First Commit of the 2nd project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flux-site-builder
---
+++ client/src/components/BackGroundImage.tsx
@@ -0,0 +1,15 @@
+const BackGroundImage = () => {
+  return (
+    <>
+      {" "}
+      {/* BACKGROUND IMAGE */}
+      <img
+        src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/hero/bg-gradient-2.png"
+        className="absolute inset-0 -z-10 size-full opacity"
+        alt=""
+      />
+    </>
+  );
+};
+
+export default BackGroundImage;
