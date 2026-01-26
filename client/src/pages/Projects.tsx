f7c8313f331c26c429eb274691ee354b9d424ee4
Daksh Yadav <dakshdav@gmail.com>
2026-01-27 01:56:48 +0530

    Something

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -57,7 +57,22 @@ const Projects = () => {
 
   const saveProject = async () => {};
   const togglePublish = async () => {};
-  const downloadCode = async () => {};
+  const downloadCode = () => {
+    const code = previewRef.current?.getCode() || project?.current_code;
+    if (!code) {
+      if (generating) {
+        return;
+      }
+      return;
+    }
+
+    const element = document.createElement("a");
+    const file = new Blob([code], { type: "text/html" });
+    element.href = URL.createObjectURL(file);
+    element.download = "index.html";
+    document.body.appendChild(element);
+    element.click();
+  };
 
   useEffect(() => {
     fetchProject();
